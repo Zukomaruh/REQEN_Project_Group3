c0b074b944ee13e3493875a96ed3c39cf0ca6e04
LukaCondric <wi24b094@gmail.com>
2025-11-18 10:27:18 +0100

    Updated the Account file

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -24,5 +24,59 @@ public class Account {
 
     }
 
+    public Long getUserId() {
+        return userId;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public SessionStatus.UserRole getRole() {
+        return role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(SessionStatus.UserRole role) {
+        this.role = role;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Account(String username, String email, String password, SessionStatus.UserRole role, boolean active) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.active = active;
+    }
 }
