f03ff4ab60576c1655d9ec694da81f05f8b0b60f
Konrad <wi24b078@technikum-wien.at>
2025-12-08 01:40:43 +0100

    stepdefinitionfiles angelegt

---
+++ src/test/java/org/example/StepDefinition_epic1_account_management.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinition_epic1_account_management {
+    @Given("The customer is on the systems main class")
+    public void theCustomerIsOnTheSystemsMainClass() {
+    }
+
+    @When("the customer registers a new account with the username {string}")
+    public void theCustomerRegistersANewAccountWithTheUsername(String arg0) {
+    }
+
+    @And("enters the email {string}")
+    public void entersTheEmail(String arg0) {
+    }
+
+    @And("enters the password {string}")
+    public void entersThePassword(String arg0) {
+    }
+
+    @And("the role is set to {string}")
+    public void theRoleIsSetTo(String arg0) {
+    }
+
+    @Then("an Account with the username {string} is successfully created.")
+    public void anAccountWithTheUsernameIsSuccessfullyCreated(String arg0) {
+    }
+
+    @And("a confirmation message is printed that says {string}")
+    public void aConfirmationMessageIsPrintedThatSays(String arg0) {
+    }
+
+    @Given("the customer is on the system main class")
+    public void theCustomerIsOnTheSystemMainClass() {
+    }
+
+    @Then("the Account is not created")
+    public void theAccountIsNotCreated() {
+    }
+
+    @And("an error message is printed that says {string}")
+    public void anErrorMessageIsPrintedThatSays(String arg0) {
+    }
+
+    @Given("An Account exists with the following user credentials:")
+    public void anAccountExistsWithTheFollowingUserCredentials() {
+    }
+
+    @When("the Account information is requested")
+    public void theAccountInformationIsRequested() {
+    }
+
+    @Then("the output is:")
+    public void theOutputIs() {
+    }
+
+    @Given("There exists an Account with the following user credentials:")
+    public void thereExistsAnAccountWithTheFollowingUserCredentials() {
+    }
+
+    @When("the email is updated to {string}")
+    public void theEmailIsUpdatedTo(String arg0) {
+    }
+}
---
+++ src/test/java/org/example/StepDefinition_epic6_manage_charging_location.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinition_epic6_manage_charging_location {
+    @Given("the owner is on the system main class.")
+    public void theOwnerIsOnTheSystemMainClass() {
+    }
+
+    @When("the owner creates a charging location with the name {string}")
+    public void theOwnerCreatesAChargingLocationWithTheName(String arg0) {
+    }
+
+    @And("enters the address {string}")
+    public void entersTheAddress(String arg0) {
+    }
+
+    @Then("a charging Location with the name {string} with an empty Charging Station List is created successfully")
+    public void aChargingLocationWithTheNameWithAnEmptyChargingStationListIsCreatedSuccessfully(String arg0) {
+    }
+
+    @Then("no charging location is created")
+    public void noChargingLocationIsCreated() {
+    }
+
+    @Given("these charging locations exist:")
+    public void theseChargingLocationsExist() {
+    }
+
+    @When("the system retrieves all charging locations")
+    public void theSystemRetrievesAllChargingLocations() {
+    }
+}
