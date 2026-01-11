4888232fc6bd5a190157fc5bfe89fbefc6c2c38c
heto66 <wi24b191@technikum-wien.at>
2026-01-12 00:30:08 +0100

    added TERMINATED Session Status

--- src/main/java/org/example/enums/SessionStatus.java
+++ src/main/java/org/example/enums/SessionStatus.java
@@ -1,7 +1,7 @@
 package org.example.enums;
 
 public enum SessionStatus {
-    ACTIVE, COMPLETED, CANCELLED, INTERRUPTED;
+    ACTIVE, COMPLETED, CANCELLED, INTERRUPTED, TERMINATED;
 
     public enum TransactionType {
         DEPOSIT, WITHDRAWAL, PAYMENT
